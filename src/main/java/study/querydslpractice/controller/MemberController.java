468f6fe649cab4ccccb89ccba2bb8d223f83ee99
insukChoi <cis1725@gmail.com>
2020-03-23 22:15:59 +0900

    조회 API 컨트롤러 개발

---
+++ src/main/java/study/querydslpractice/controller/MemberController.java
@@ -0,0 +1,23 @@
+package study.querydslpractice.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import study.querydslpractice.dto.MemberSearchCondition;
+import study.querydslpractice.dto.MemberTeamDto;
+import study.querydslpractice.repository.MemberJpaRepository;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberJpaRepository memberJpaRepository;
+
+    @GetMapping("/v1/members")
+    public List<MemberTeamDto> searchMemberV1(MemberSearchCondition condition) {
+        return memberJpaRepository.search(condition);
+    }
+}
+
