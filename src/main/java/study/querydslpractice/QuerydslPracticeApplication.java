8ffdd9e3bf78bf20899de6bb1136e35df66d36ad
insukChoi <cis1725@gmail.com>
2020-03-23 21:58:27 +0900

    동적 쿼리 성능 최적화 조회

--- src/main/java/study/querydslpractice/QuerydslPracticeApplication.java
+++ src/main/java/study/querydslpractice/QuerydslPracticeApplication.java
@@ -1,7 +1,11 @@
 package study.querydslpractice;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.persistence.EntityManager;
 
 @SpringBootApplication
 public class QuerydslPracticeApplication {
@@ -9,5 +13,4 @@ public class QuerydslPracticeApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(QuerydslPracticeApplication.class, args);
 	}
-
 }
